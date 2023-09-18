833c01f18579b9f0b0651c605c305917aa1b7cb2
kapysta <kapysta000017@gmail.com>
2023-09-18 19:52:29 +0300

    added pleer vidstack.io

--- src/App.js
+++ src/App.js
@@ -4,6 +4,16 @@ import React from "react"
 import video from "./he.mp4"
 import img from "./hello.jpg"
 
+import "vidstack/styles/defaults.css"
+import "vidstack/styles/community-skin/video.css"
+
+import {
+  MediaCommunitySkin,
+  MediaOutlet,
+  MediaPlayer,
+  MediaPoster,
+} from "@vidstack/react"
+
 function App() {
   const videoJsOptions = {
     controls: true,
@@ -44,6 +54,36 @@ function App() {
           type="video/mp4"
           controls
         />
+      </div>
+
+      <h1>library Vidstack.io</h1>
+      <div style={{ width: "640px", height: "480px", margin: "0 auto" }}>
+        <MediaPlayer
+          title="Sprite Fight"
+          src={video}
+          poster={img}
+          // thumbnails="https://media-files.vidstack.io/sprite-fight/thumbnails.vtt"
+          aspectRatio={16 / 9}
+          crossorigin=""
+        >
+          <MediaOutlet>
+            <MediaPoster alt="Girl walks into sprite gnomes around her friend on a campfire in danger!" />
+            {/* <track
+              src="https://media-files.vidstack.io/sprite-fight/subs/english.vtt"
+              label="English"
+              srcLang="en-US"
+              kind="subtitles"
+              default
+            /> */}
+            {/* <track
+              src="https://media-files.vidstack.io/sprite-fight/chapters.vtt"
+              srcLang="en-US"
+              kind="chapters"
+              default
+            /> */}
+          </MediaOutlet>
+          <MediaCommunitySkin />
+        </MediaPlayer>
       </div>
     </div>
   )
